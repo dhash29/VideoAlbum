e0229623a7ae06504301e157622cba857fdcd3dc
dhash29 <103362548+dhash29@users.noreply.github.com>
2023-06-17 11:22:59 +1000

    Add files via upload

---
+++ video.py
@@ -0,0 +1,32 @@
+import os
+import cv2
+
+pathes = "Images"
+
+images = []
+
+for file in os.listdir(pathes):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = pathes+"/"+file
+               
+        images.append(file_name)
+        
+
+count = len(images)
+
+
+frame = cv2.imread(images[0])
+
+height, width, channels = frame.shape
+
+size = (width, height)
+
+out = cv2.VideoWriter("Video.avi", cv2.VideoWriter_fourcc(*'DIVX'), 0.8, size)
+
+for i in range(0, count-1):
+    frame = cv2.imread(images[i])
+    out.write(frame)
+
+out.release()
